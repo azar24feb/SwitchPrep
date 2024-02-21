42edfa238c3e4822ab24e93ae1cd023325c95c3e
azar <azar24feb@gmail.com>
2024-02-21 22:16:28 +0530

    default commit message

--- src/striver/array/ArrayMedium.java
+++ src/striver/array/ArrayMedium.java
@@ -11,6 +11,19 @@ public class ArrayMedium {
     }
 
     /**
+     * Sort 0,1,2
+     * The steps will be the following:
+     *
+     * First, we will run a loop that will continue until mid <= high.
+     * There can be three different values of mid pointer i.e. arr[mid]
+     * If arr[mid] == 0, we will swap arr[low] and arr[mid] and will increment both low and mid. Now the subarray from index 0 to (low-1) only contains 0.
+     * If arr[mid] == 1, we will just increment the mid pointer and then the index (mid-1) will point to 1 as it should according to the rules.
+     * If arr[mid] == 2, we will swap arr[mid] and arr[high] and will decrement high. Now the subarray from index high+1 to (n-1) only contains 2.
+     * In this step, we will do nothing to the mid-pointer as even after swapping, the subarray from mid to high(after decrementing high)
+     *          might be unsorted. So, we will check the value of mid again in the next iteration.
+     */
+
+    /**
      *Count Subarray sum Equals K
      * Problem Statement: Given an array of integers and an integer k, return the total number of subarrays whose sum equals k.
      */
